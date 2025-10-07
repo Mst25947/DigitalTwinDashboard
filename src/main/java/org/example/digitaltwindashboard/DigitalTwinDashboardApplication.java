4dc8c85cd80eff21c650f15ac0b15fccfea96b10
MaxxieMax1 <max.breunesse@gmail.com>
2025-10-07 14:01:40 +0200

    Initial commit

---
+++ src/main/java/org/example/digitaltwindashboard/DigitalTwinDashboardApplication.java
@@ -0,0 +1,13 @@
+package org.example.digitaltwindashboard;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DigitalTwinDashboardApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(DigitalTwinDashboardApplication.class, args);
+    }
+
+}
---
+++ src/test/java/org/example/digitaltwindashboard/DigitalTwinDashboardApplicationTests.java
@@ -0,0 +1,13 @@
+package org.example.digitaltwindashboard;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DigitalTwinDashboardApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
