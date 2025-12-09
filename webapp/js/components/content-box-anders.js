1da40d09e394a33be4c6cb275e1d06d1dc6980ea
Mst25947 <matthijs.strang@student.hu.nl>
2025-12-09 20:43:06 +0100

    list veranderd

--- webapp/js/components/content-box-anders.js
+++ webapp/js/components/content-box-anders.js
@@ -19,14 +19,41 @@ export class ContentBoxAnders extends LitElement {
         flex-direction: column;
         gap: 10px; 
     }
+
+    ul {
+    list-style-type: none;
+    padding: 0;
+    margin: 0;
+}
+    li {
+        margin-bottom: 8px;
+        font-size: 16px;
+    }
+        
+
+  #title {
+        font-weight: bold;
+        font-size: 24px;
+        margin-bottom: 10px;
+    }
+        #cats {
+        font-size: 20px;
+        font-weight: bold;
+}
+          #desc {
+        font-size: 16px;
+        margin-bottom: 15px;
+    }
   `;
 
 
     render() {
         return html`
       <div id="content-box">
-      <p>BOB-Input</p>
+      <p id="title">BOB-Input</p>
+      <p id="desc">In de onderstaande lijst staan de verschillende categorieën die in de BOB-tool worden meegenomen voor de analyse van het geselecteerde gebied:</p>
       <ul>
+        <p id="cats">Categorieën:</p>
         <li>Straat & Stoep</li>
         <li>Wooneenheden</li>
         <li>Parkeren</li>
