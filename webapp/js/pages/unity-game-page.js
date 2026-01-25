21cc7dd9256dc3da2e9c26d5dac6e21bea66de92
Mst25947 <144690575+Mst25947@users.noreply.github.com>
2026-01-25 19:16:31 +0100

    test fix

--- webapp/js/pages/unity-game-page.js
+++ webapp/js/pages/unity-game-page.js
@@ -22,7 +22,7 @@ class UnityGamePage extends LitElement {
   render() {
     return html`
       <div class="container">
-        <iframe src="/js/pages/TestBuild/index.html"></iframe>
+        <iframe src="/TestBuild/index.html"></iframe>
       </div>
     `;
   }
